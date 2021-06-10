2691d93b07a5f1bb9b00df4c3dc658f98de1a49a
Aleksey Koleznev <fc.fenomen@mail.ru>
2021-06-10 14:02:39 +0700

    Preparation before publication.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const hotelRoute = require('./routes/hotel');
 const conveniencesRoute = require('./routes/conveniences');
@@ -20,6 +21,14 @@ app.use(require('cors')());
 
 app.use('/api/hotel', hotelRoute);
 app.use('/api/conveniences', conveniencesRoute);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 
 module.exports = app;
